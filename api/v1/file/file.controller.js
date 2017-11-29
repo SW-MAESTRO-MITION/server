d90b913a7b83b94b39ad93b3af09b5fe6772ce6d
Sangwon Hong <qpakzk@gmail.com>
2017-11-29 18:48:51 +0900

    add js-sha256 library
    Signed-off-by: Sangwon Hong <qpakzk@gmail.com>

--- api/v1/file/file.controller.js
+++ api/v1/file/file.controller.js
@@ -1,7 +1,7 @@
 var multer = require('multer');
 var upload = multer({ dest: 'uploads/' })
 
-exports.uploadFile = function (req, res){
+exports.uploadFile = function (req, res) {
   res.status(200).json(req.file);
 };
 
