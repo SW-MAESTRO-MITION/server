85b65cb80097266aad9054d737fd3614d725d557
Sangwon Hong <qpakzk@gmail.com>
2017-12-10 00:56:47 +0900

    modify identations and add console.log() to print out for testing the objects
    Signed-off-by: Sangwon Hong <qpakzk@gmail.com>

--- api/v1/user/user.controller.js
+++ api/v1/user/user.controller.js
@@ -4,92 +4,100 @@ var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 var User = require('../../../models/user');
 
 function createUser(req, res) {
-    var user = new User();
-    user.name = req.body.name;
-    user.email = req.body.email;
-    user.password = req.body.password;
+  var user = new User();
+  user.name = req.body.name;
+  user.email = req.body.email;
+  user.password = req.body.password;
 
-    user.save((err, createdUser) => {
-        if (err) {
-            res.status(500).json(err);
-            console.log(err.message);
-        }
-        res.status(200).json(createdUser);
-    });
+  user.save(function(err, createdUser) {
+    if (err) {
+      res.status(500).json(err);
+      console.log(err.message);
+    }
+
+    console.log(createdUser);
+    res.status(200).json(createdUser);
+  });
 }
 
 function getUser(req, res) {
-    User.findOne({"_id": req.params.id}, (err, user) => {
-        if (err) {
-            res.status(500).json(err);
-        }
+  User.findOne({"_id": req.params.id}, function(err, user) {
+    if (err) {
+      res.status(500).json(err);
+    }
 
-        if (user) {  // Search could come back empty, so we should protect against sending nothing back
-            res.status(200).json(user);
-        } else {  // In case no kitten was found with the given query
-            res.status(200).json("No user found");
-        }
-    });
+    if (user) {
+      console.log(user);
+      res.status(200).json(user);
+    }
+    else {
+      res.status(200).json("User is not found.");
+    }
+  });
 };
 
 function login(req, res) {
-    User.findOne({
-        "email": req.body.email,
-        "password": req.body.password
-    }, (err, user) => {
-        if (err) {
-            res.status(500).json(err);
-        }
-        if (user) {
-            res.status(200).json(user);
-        } else {
-            res.status(200).json("No user found");
-        }
-    });
+  User.findOne({
+    "email": req.body.email,
+    "password": req.body.password
+  }, function(err, user) {
+    if (err) {
+      res.status(500).json(err);
+    }
+
+    if (user) {
+      console.log(user);
+      res.status(200).json(user);
+    }
+    else {
+      res.status(200).json("User is not found.");
+    }
+  });
 }
 
 function modifyUser(req, res) {
-    User.findById(req.params.id, (err, user) => {
-        // Handle any possible database errors
-        if (err) {
-            res.status(500).send(err);
-        } else {
-            // Update each attribute with any possible attribute that may have been submitted in the body of the request
-            // If that attribute isn't in the request body, default back to whatever it was before.
-            user.name = req.body.name || user.name;
-            user.email = req.body.email || user.email;
-            user.password = req.body.password || user.password;
-            user.is_checked_registered_contract = req.body.is_checked_registered_contract || user.is_checked_registered_contract;
+  User.findById(req.params.id, function(err, user) {
+    if (err) {
+      res.status(500).send(err);
+    }
+    else {
+      user.name = req.body.name || user.name;
+      user.email = req.body.email || user.email;
+      user.password = req.body.password || user.password;
+      user.is_checked_registered_contract = req.body.is_checked_registered_contract || user.is_checked_registered_contract;
 
-            // Save the updated document back to the database
-            user.save((err, modifiedUser) => {
-                if (err) {
-                    res.status(500).send(err)
-                }
-                res.status(200).json(modifiedUser);
-            });
-        }
-    });
+      user.save(function(err, modifiedUser) {
+      if (err) {
+        res.status(500).send(err)
+      }
+
+        console.log(modifiedUser);
+        res.status(200).json(modifiedUser);
+      });
+    }
+  });
 }
 
 function deleteUser(req, res) {
-    User.findByIdAndRemove(req.params.id, (err, user) => {
-        if (err) {
-            res.status(500).send(err)
-        }
+  User.findByIdAndRemove(req.params.id, (err, user) => {
+    if (err) {
+    res.status(500).send(err)
+    }
 
-        let response = {
-            message: "Todo successfully deleted",
-            id: user._id
-        };
-        res.status(200).json(response);
-    });
+    let response = {
+    message: "User is successfully deleted.",
+    id: user._id
+    };
+
+    console.log(response);
+    res.status(200).json(response);
+  });
 }
 
 module.exports = {
-    createUser,
-    login,
-    getUser,
-    modifyUser,
-    deleteUser
+  createUser,
+  login,
+  getUser,
+  modifyUser,
+  deleteUser
 }
