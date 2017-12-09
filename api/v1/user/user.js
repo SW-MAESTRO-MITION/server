fd8211a270dc4f782285df5886f58c655d183774
Sangwon Hong <qpakzk@gmail.com>
2017-12-10 01:06:30 +0900

    add the validation of passwords
    Signed-off-by: Sangwon Hong <qpakzk@gmail.com>

--- api/v1/user/user.js
+++ api/v1/user/user.js
@@ -3,10 +3,10 @@ var router = express.Router();
 var user = require('./user.controller');
 var validateParams = require('./user.middleware');
 
-router.post('/', user.createUser);
+router.post('/', validateParams.validatePassword, user.createUser);
 router.post('/login', user.login);
 router.get('/:id', user.getUser);
-router.put('/:id', user.modifyUser);
+router.put('/:id', validateParams.validatePassword, user.modifyUser);
 router.delete('/:id', user.deleteUser);
 
 module.exports = router;
