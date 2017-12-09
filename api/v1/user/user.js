927c3ad32fde3e3645f14e6713e6fd30352516f5
Sangwon Hong <qpakzk@gmail.com>
2017-12-10 01:00:25 +0900

    change the order
    Signed-off-by: Sangwon Hong <qpakzk@gmail.com>

--- api/v1/user/user.js
+++ api/v1/user/user.js
@@ -3,10 +3,10 @@ var router = express.Router();
 var user = require('./user.controller');
 var validateParams = require('./user.middleware');
 
+router.post('/', user.createUser);
 router.post('/login', user.login);
 router.get('/:id', user.getUser);
 router.put('/:id', user.modifyUser);
 router.delete('/:id', user.deleteUser);
-router.post('/', user.createUser);
 
 module.exports = router;
