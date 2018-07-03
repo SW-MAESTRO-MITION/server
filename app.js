c48b22ab1ecb8863c8533ab0aea5ee2da0facc56
Sangwon Hong <qpakzk@gmail.com>
2018-07-03 17:20:16 +0900

    Refactoring app.j

--- app.js
+++ app.js
@@ -1,33 +1,28 @@
-var express = require('express');
-var path = require('path');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 
-var user = require('./api/user/user');
-var contract = require('./api/contract/contract');
-var file = require('./api/file/file');
-var mailing = require('./api/mailing/mailing');
-var transaction = require('./api/transaction/transaction');
+const user = require('./api/user/user');
+const contract = require('./api/contract/contract');
+const file = require('./api/file/file');
+const mailing = require('./api/mailing/mailing');
+const transaction = require('./api/transaction/transaction');
 
-var app = express();
-var mongoose = require('mongoose');
-var db = mongoose.connection;
+const app = express();
+const mongoose = require('mongoose');
+const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
     console.log('connect successfully');
 });
 mongoose.connect('mongodb://localhost/mongodb_tutorial', { useMongoClient: true });
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/user', user);
 app.use('/contract', contract);
@@ -37,14 +32,14 @@ app.use('/mailing', mailing);
 app.use('/transaction', transaction);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
