48799fd759600aaa7cd1d75b03cce35bfa66b878
Sangwon Hong <qpakzk@gmail.com>
2018-07-03 20:36:15 +0900

    Refactoring user middleware

--- api/user/user.middleware.js
+++ api/user/user.middleware.js
@@ -1,11 +1,16 @@
-exports.validatePassword = function(req, res, next) {
-  var name = req.body.name;
-  var password = req.body.password;
+validatePassword = (req, res, next) => {
+  const password = req.body.password;
 
   if(!password || password.length < 3) {
-    return res.status(400).json({code : -1,
-                  message: 'password is something wrong!'});
-  } else{
+    return res.status(400).json({
+      code : -1,
+      message: 'password is something wrong!'});
+  }
+  else {
       return next();
   }
+}
+
+module.exports = {
+  validatePassword
 }
